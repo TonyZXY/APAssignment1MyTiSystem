cdcc1f4687ed0d8e4fce23c070d947e64e0d43a9
TonyZXY <xyzheng109@gmail.com>
2016-09-05 19:01:02 +1000

    make changes

--- src/MyTiSystem.java
+++ src/MyTiSystem.java
@@ -335,6 +335,10 @@ public class MyTiSystem {
         }
     }
 
+    public void journeyPurchase(){
+
+    }
+
     private void travelPassPurchase() { //check user info
         String userID;
         printBlackLine();
